288a7cd308bd740cc2cd6fb28875ba08bf7545cb
kitt mattison <kittmatt1@gmail.com>
2020-03-29 00:14:45 -0700

    add-train-buttom

--- assets/logic.js
+++ assets/logic.js
@@ -10,6 +10,8 @@ var timeRemaining = "";
 
 var cTrain = $("#train-title");
 var cdestination = $("#train-destination");
+var cTrainTime = $("#train-time")
+var cTimeFreq = $("#time-freq")
 
 //call firebase
      var firebaseConfig = {
@@ -26,7 +28,7 @@ firebase.initializeApp(firebaseConfig);
         
 var db = firebase.database();
 
-database.ref("/trains").on("child_added", function(snapshot) {
+database.ref("/trains").on("child_added", function (snapshot) {
      //local variables
      var diff = 0;
      var remainder = 0;
@@ -43,18 +45,57 @@ database.ref("/trains").on("child_added", function(snapshot) {
      //format for standard time
      nextrain = moment().add(arrivinging, "m").format("hh:mm A");
 
-    // append to our table of trains, inside tbody, with a new row of the train data
-    $("#table-data").append(
-        "<tr><td>" + snapshot.val().name + "</td>" +
-        "<td>" + snapshot.val().destination + "</td>" +
-        "<td>" + frequency + "</td>" +
-        "<td>" + arrivinging + "</td>" +
-        "<td>" + nextrain + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
-    );
+     // append to our table of trains, inside tbody, with a new row of the train data
+     $("#table-data").append(
+          "<tr><td>" + snapshot.val().name + "</td>" +
+          "<td>" + snapshot.val().destination + "</td>" +
+          "<td>" + frequency + "</td>" +
+          "<td>" + arrivinging + "</td>" +
+          "<td>" + nextrain + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+     );
+     console.log("#table-data");
 
-    $("span").hide();
+     $("span").hide();
 
   
 });
+
+var saveinputs = function (event) {
+     event.prevenDefault();
+
+     trainName = cTrain.val().trim();
+     destination = cdestination.val().trim();
+     trainTime = moment(cTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+     frequency = cTimeFreq.val().trim();
+
+     db.ref("/trains").push({
+          name: trainName,
+          destination: destination,
+          time: trainTime,
+          frequency: frequency,
+          NextArrivial: nextrain,
+          mininutesAway: arrivinging,
+          date_added: firebase.database.servervalue.TIMESTAMP
+     });
+
+     alert("train added");
+
+     ///empty form
+     cTrain.val("");
+     cdestination.val("");
+     cTrainTime.val("");
+     cTimeFreq.val("");
+};
+
+$("btn-add").on("click", function (event) {
+     if (cTrain.val().length === 0 || cdestination.val().length === 0 || cTrainTime.val().length === 0 || cTimeFreq === 0) {
+          alert("missing fields");
+     } else {
+          saveinputs(event);
+     }
+     
+});
+
+
      
      
